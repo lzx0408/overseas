c4da4b2922d6bc2c531c7a7e29561b651e1c72b2
li <1223757604@qq.com>
2021-11-04 17:46:28 +0800

    第四部分

---
+++ src/views/abroad/abroad.vue
@@ -0,0 +1,485 @@
+<template>
+  <div class="abroad">
+    <div class="wrap">
+      <header class="header">
+        <h2>中外合作留学</h2>
+      </header>
+      <main>
+        <div class="tabs">
+          <ul>
+            <li>留学预科</li>
+            <li>2+2留学</li>
+            <li>3+2留学</li>
+          </ul>
+        </div>
+        <div class="wrap_dv">
+          <div class="wrap_tit">
+            <div class="dao">| 什么是留学预科</div>
+            <div class="tit">
+              留学预科是指读大学前的预备教育，一般都要求海外高中毕业生或专本科毕业生读1年左右的预科，以适应国外的学习方式。
+            </div>
+            <ul>
+              <li class="list">
+                <div class="dv">学习方式</div>
+                <span> 国内1年,国外3-4年；获国外学士学位</span>
+              </li>
+              <li class="list">
+                <div class="dv">国内费用</div>
+                <span style="color: #cc8b59"> 6-9万元</span>/年
+              </li>
+              <li class="list">
+                <div class="dv">国外费用</div>
+                <span style="color: #cc8b59"> 30-35万元</span>/年
+              </li>
+              <li class="list">
+                <div class="dv">招生对象</div>
+                <span> 应往届高中毕业生、专/本科毕业生</span>
+              </li>
+              <li class="list">
+                <div class="dv">申请条件</div>
+                <span> 通过笔试+面试</span>
+              </li>
+              <li class="list">
+                <div class="dv">留学国家</div>
+                <span> 英国、美国、澳大利亚、加拿大等</span>
+              </li>
+              <li class="list">
+                <div class="dv">留学专业</div>
+                <span> 会计、金融、艺术、设计等</span>
+              </li>
+            </ul>
+            <div class="btn">进入留学预科频道</div>
+          </div>
+        </div>
+
+        <div class="wrap_dv2">
+          <div class="move">
+            <div class="move_tou">
+              <span>留学预科热招院校</span>
+              <div class="btnd">更多></div>
+            </div>
+            <div class="move_top">
+              <ul class="ullist">
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/15560105665cbed646ad65c.jpg"
+                    alt=""
+                  />
+                  <span>北京外国语大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/16279653696108c7b937dec.jpg"
+                    alt=""
+                  />
+                  <span>中央财经大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/15560105665cbed646ad65c.jpg"
+                    alt=""
+                  />
+                  <span>北京外国语大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/16279653696108c7b937dec.jpg"
+                    alt=""
+                  />
+                  <span>中央财经大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/15560105665cbed646ad65c.jpg"
+                    alt=""
+                  />
+                  <span>北京外国语大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/16279653696108c7b937dec.jpg"
+                    alt=""
+                  />
+                  <span>中央财经大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/15560105665cbed646ad65c.jpg"
+                    alt=""
+                  />
+                  <span>北京外国语大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/16279653696108c7b937dec.jpg"
+                    alt=""
+                  />
+                  <span>中央财经大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/15560105665cbed646ad65c.jpg"
+                    alt=""
+                  />
+                  <span>北京外国语大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/16279653696108c7b937dec.jpg"
+                    alt=""
+                  />
+                  <span>中央财经大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/15560105665cbed646ad65c.jpg"
+                    alt=""
+                  />
+                  <span>北京外国语大学出国留学</span>
+                </li>
+                <li class="lilist">
+                  <img
+                    src="http://xht.yuloo.com/Public/Uploads/16279653696108c7b937dec.jpg"
+                    alt=""
+                  />
+                  <span>中央财经大学出国留学</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="wrap_dv3">
+          <ul class="chu_li">
+            <li class="chu_o"><a href="">中外合作留学择校指南</a></li>
+            <li class="chu_t"><a href="">中外合作留学择校指南</a></li>
+          </ul>
+          <div class="chu_chong">
+            <ul class="chu_w">
+              <li>
+                <a href="" class="chu_a"
+                  >申请中外合作办学留学需要满足什么条件？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >北京什么学校的中外合作办学2+2留学比较好？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >申请中外合作办学留学需要满足什么条件？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >北京什么学校的中外合作办学2+2留学比较好？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >申请中外合作办学留学需要满足什么条件？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >北京什么学校的中外合作办学2+2留学比较好？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >申请中外合作办学留学需要满足什么条件？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >北京什么学校的中外合作办学2+2留学比较好？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >申请中外合作办学留学需要满足什么条件？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >北京什么学校的中外合作办学2+2留学比较好？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >申请中外合作办学留学需要满足什么条件？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+              <li>
+                <a href="" class="chu_a"
+                  >北京什么学校的中外合作办学2+2留学比较好？</a
+                >
+                <span class="chu_shu">10-28</span>
+              </li>
+            </ul>
+            <a href="" class="chu_hu">查看更多中外合作留学择校指南>></a>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  font-size: 0.14rem;
+}
+
+html {
+  width: 100%;
+  height: 100%;
+  min-width: 3.75rem;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}
+.abroad {
+  width: 3.75rem;
+  background: #f5f5f5;
+}
+.wrap {
+  width: 3.56rem;
+  height: 16.5rem;
+  margin: 0 auto;
+  background: #fff;
+}
+.header {
+  width: 3.56rem;
+  height: 0.54rem;
+  background: #fff;
+  h2 {
+    float: left;
+    position: relative;
+    background: #36477a;
+    color: #fff;
+    font-size: 0.22rem;
+    line-height: 0.3rem;
+    font-weight: bold;
+    padding: 0.12rem 0.15rem;
+  }
+}
+.tabs {
+  width: 3.56rem;
+  height: 0.43rem;
+  background: #fff;
+  margin: 5% 0;
+  ul {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    li {
+      width: 1.11rem;
+      height: 0.32rem;
+      border: 0.01rem solid #cc8b59;
+      font-size: 0.14rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+.wrap_dv {
+  width: 3.24rem;
+  height: 4.81rem;
+  margin: 0 auto;
+  background: #f6f8fd;
+  padding: 0.05rem 0.05rem;
+  .wrap_tit {
+    width: 2.92rem;
+    margin: 0 auto;
+    .dao {
+      font-size: 0.16rem;
+      color: #36477a;
+      display: flex;
+      padding: 0.15rem 0.05rem;
+    }
+    .tit {
+      font-size: 0.14rem;
+      color: #36477a;
+      line-height: 0.24rem;
+      padding: 0.05rem 0.13rem;
+      border-radius: 0.04rem;
+      overflow: hidden;
+      margin-bottom: 0.16rem;
+      background: #e6ebf8;
+    }
+    .list {
+      display: flex;
+      font-size: 0.14rem;
+      padding: 0.06rem 0.05rem;
+      span {
+        font-size: 0.14rem;
+        display: flex;
+      }
+      .dv {
+        width: 0.76rem;
+        height: 0.19rem;
+        font-size: 0.14rem;
+        border: 0.01rem solid #36477a;
+      }
+    }
+  }
+}
+.btn {
+  width: 2.7rem;
+  height: 0.35rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.15rem;
+  color: #36477a;
+  border: 0.01rem solid #36477a;
+}
+
+.wrap_dv2 {
+  width: 3.24rem;
+  border: 0.01rem solid #e7e7e7;
+  padding: 0.05rem 0.05rem;
+  margin: 3%;
+  .move {
+    width: 2.92rem;
+    margin: 0 auto;
+    .move_tou {
+      height: 0.2rem;
+      padding: 0.15rem 0.05rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      span {
+        font-size: 0.16rem;
+      }
+      .btnd {
+        font-size: 0.12rem;
+        border: 0.01rem solid #bbbbbb;
+        width: 0.58rem;
+        height: 0.18rem;
+        color: #bbbbbb;
+        border-radius: 0.16rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+    .move_top {
+      .ullist {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        .lilist {
+          width: 1.35rem;
+          height: 0.68rem;
+          border: 0.01rem solid #ccc;
+          font-size: 0.14rem;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          border-radius: 0.04rem;
+          margin-top: 0.05rem;
+          img {
+            width: 0.36rem;
+            height: 0.36rem;
+          }
+          span {
+            width: 0.82rem;
+            height: 0.52rem;
+            font-size: 0.14rem;
+          }
+        }
+      }
+    }
+  }
+}
+.chu_li {
+  display: flex;
+  margin-bottom: 0.1rem;
+}
+.chu_li li {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  width: 1.63rem;
+  height: 0.32rem;
+}
+.chu_o {
+  border-radius: 0.04rem 0.04rem 0 0;
+  font-weight: bold;
+  color: #fff !important;
+  background: #cc8b59 !important;
+  border-color: #cc8b59 !important;
+}
+.chu_o a {
+  color: #fff;
+}
+.chu_t {
+  border: 1px solid #ddd;
+  background: #fff;
+}
+.chu_t a {
+  color: #333;
+}
+h2 {
+  margin-bottom: 0.15rem;
+}
+.i {
+  padding: 0.02rem 0.15rem;
+  background: url(../../assets/icon.png) no-repeat center;
+  background-position: -0.04rem -0.49rem;
+}
+.chu_chong {
+  width: 3.26rem;
+}
+.chu_w li {
+  display: flex;
+  width: 100%;
+  height: 0.3rem;
+  padding: 0.02rem 0;
+}
+.chu_a {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+  height: 0.2rem;
+  width: 2.76rem;
+  color: #555;
+}
+.wrap_dv3 {
+  width: 3.24rem;
+  height: 4.73rem;
+  border: 0.01rem solid #e7e7e7;
+  padding: 0.05rem 0.05rem;
+  margin: 3%;
+  .chu_hu {
+    color: #36477a;
+  }
+}
+</style>
